be8a1829df5c3ea2fa81c1f2f4161d18b1f3c69c
ashikarjen <ashikarjen1234@gmail.com>
2026-06-26 02:54:34 +0600

    added custom user model and simple JWT

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "email",
+        "username",
+        "is_verified",
+        "is_staff",
+    )
+
+    search_fields = (
+        "email",
+        "username",
+    )
+
+    ordering = ("email",)
--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,25 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    email = models.EmailField(unique=True)
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+
+    profile_image = models.ImageField(
+        upload_to="users/",
+        blank=True,
+        null=True,
+    )
+
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
