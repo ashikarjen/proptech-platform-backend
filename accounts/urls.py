8aca98e19e2b863ec8d7628dec09916963229ebc
ashikarjen <ashikarjen1234@gmail.com>
2026-06-27 06:23:08 +0600

    Implement user registration API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
+from .views import RegisterAPIView
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path(
+        "register/",
+        RegisterAPIView.as_view(),
+        name="register",
+    ),
 ]
